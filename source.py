654f295d181ff969f6eda961739d483c0a18bbd7
alisayar <104276323+alisa-yar@users.noreply.github.com>
2023-02-06 03:49:05 +0200

    Update source.py

--- source.py
+++ source.py
@@ -1,4 +1,14 @@
+import urllib.request 
 from urllib.request import urlopen
-html = urlopen("http://www.google.com/")
+html = urlopen("https://www.google.com/")
 content = html.read()
 print(content)
+
+
+request_url = urllib.request.urlopen('https://www.geeksforgeeks.org/') 
+print(request_url.read()) 
+
+
+# install it
+urllib.request.install_opener(opener)
+f = urllib.request.urlopen('https://www.python.org/')
