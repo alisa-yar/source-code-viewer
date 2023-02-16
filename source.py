95da5d7dc1e8f802d249ea44dab3e97bf7f426aa
alisayar <104276323+alisa-yar@users.noreply.github.com>
2023-02-16 04:33:17 +0200

    Update source.py

--- source.py
+++ source.py
@@ -4,12 +4,12 @@ html = urlopen("https://www.google.com/")
 content = html.read()
 print(content)
 
-
+# or
 request_url = urllib.request.urlopen('https://www.geeksforgeeks.org/') 
 print(request_url.read()) 
 
 
-# install it
+# or
 urllib.request.install_opener(opener)
 f = urllib.request.urlopen('https://www.python.org/')
 print(f)
